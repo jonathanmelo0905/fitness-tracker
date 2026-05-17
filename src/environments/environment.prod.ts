143c6495e7c0f7a4b49b8b5ae6235eccc4a1f4ea
jonathanmelo0905 <jonathanmelo0905@gmail.com>
2026-05-16 23:06:10 -0500

    chore(config): add appName and appVersion to environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  appName: 'NutriEval',
+  appVersion: '1.1',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  appName: 'NutriEval',
+  appVersion: '1.1',
 };
 
 /*
